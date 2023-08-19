948f57db269f544f52b5fecf5020abdcd516a840
xxw11 <918815989@qq.com>
2023-08-19 18:19:20 +0800

    modify the trainning script

--- train_scripts/vit/cifar_100/train_ssf_rp.sh
+++ train_scripts/vit/cifar_100/train_ssf_rp.sh
@@ -1,5 +1,6 @@
 
 source DATA_PATH.sh
+METHOD="ssf_rp_drop"
 CUDA_VISIBLE_DEVICES=0,1,2,3  python  -u -m torch.distributed.launch --nproc_per_node=4  --master_port=12345  \
 	train.py ${CIFAR100_PATH}/ --dataset torch/cifar100 --num-classes 100 --model vit_base_patch16_224_in21k  \
     --batch-size 32 --epochs 100 \
@@ -8,10 +9,11 @@ CUDA_VISIBLE_DEVICES=0,1,2,3  python  -u -m torch.distributed.launch --nproc_per
     --lr 1e-3 --min-lr 1e-8 \
     --drop-path 0 --img-size 224 \
 	--model-ema --model-ema-decay 0.99992  \
-	--output  ${OUTPUT_PATH}/vit_base_patch16_224_in21k/cifar_100/ssf_rp_drop \
+	--output  ${OUTPUT_PATH}/vit_base_patch16_224_in21k/cifar_100/${METHOD} \
 	--amp --tuning-mode ssf --pretrained --no-save --seed -1\
-    --use-relax-param --reparam-interval 50  --reparam-init-method constant\
-    --reparam-way drop --drop-rate 0.1 \
+    --use-relax-param --reparam-interval 50 \
+    --reparam-way drop --drop-rate $1 \
+    --checkpoint-hist 1 
 
 # source DATA_PATH.sh
 # CUDA_VISIBLE_DEVICES=0,1,2,3  python  -u -m torch.distributed.launch --nproc_per_node=4  --master_port=12346  \
